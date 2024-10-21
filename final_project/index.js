58be3f4112fcd831081b82ab547e456aa31eff0b
Raghvendra Bhardwaj <raghvendra14bhardwaj@gmail.com>
2024-10-21 07:18:58 -0400

    completed till task 6

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,23 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        // Verify the JWT token
+        jwt.verify(token, "your_secret_key", (err, user) => {
+            if (err) {
+                // If the token is invalid or expired, respond with a 403 error
+                return res.status(403).json({ message: "Unauthorized" });
+            }
+            // If the token is valid, save the user information to req and proceed
+            req.user = user;
+            next();
+        });
+    } else {
+        // If no token is found in the session, respond with a 403 error
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
